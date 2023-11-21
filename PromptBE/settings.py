c78eaf3a5d162113313f51c831562da96a773ab4
unknown <bayodeiretomiwa@gmail.com>
2023-11-21 03:21:08 +0100

    fix: fixed deouple issue in the settings.py

--- PromptBE/settings.py
+++ PromptBE/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 import dj_database_url
-from decouple import Config, Csv, config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # Load environment variables from .env
-config = Config()
-config.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
