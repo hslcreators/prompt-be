e917c0cd8b1fed10f2e235eb2f732e54dfafe258
Marvellous <itzmhizterlouis@gmail.com>
2023-12-09 19:23:46 +0100

    Wrote the endpoints for logging in and verifying otp

--- apps/authentication/services.py
+++ apps/authentication/services.py
@@ -1,12 +1,21 @@
 from random import randrange
+import datetime
+from rest_framework.exceptions import AuthenticationFailed
 
 
 def generate_pin():
     return randrange(1000, 10000)
 
 
-def is_otp_the_same(otp, compared):
-    if otp == compared:
-        return True;
+def verify_otp(otp, otp_serializer, request_otp):
+    # This is for changing time to get the difference in time
+    otp.dummy = "ct"
+    otp.save()
 
-    return False
+    if (otp.expiry_date - otp.created) > datetime.timedelta(minutes=3):
+        raise AuthenticationFailed("Token is Expired")
+
+    otp_pin = otp_serializer.data["otp"]
+
+    if int(otp_pin) != int(request_otp):
+        raise AuthenticationFailed("OTP is is not the same")
--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("sign-up/", views.create_user, name="Sign Up")
+    path("sign-up/", views.create_user, name="Sign Up"),
+    path("login/", views.login, name="Log in"),
+    path("verify-otp/", views.verify_token, name="Verify Token")
 ]
