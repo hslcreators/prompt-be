a3f52843d31e4a5199cfc4356c3644a9ca630eb5
Marvellous <itzmhizterlouis@gmail.com>
2023-12-09 19:26:03 +0100

    Minor Change

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("sign-up/", views.create_user, name="Sign Up"),
     path("login/", views.login, name="Log in"),
-    path("verify-otp/", views.verify_token, name="Verify Token")
+    path("verify-otp/", views.verify_token, name="Verify OTP")
 ]
