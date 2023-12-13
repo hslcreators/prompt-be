45a7f9445f5052f4b80f615d098dafcd3a18fd66
TSavage101 <bayodeiretomiwa@gmail.com>
2023-12-13 01:52:57 +0100

    refactor: added printer model to admin

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -4,12 +4,14 @@ from django.contrib.admin import AdminSite
 
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, OneTimePassword
+from .models import User, OneTimePassword, Printer
 
 from apps.core.admin import prompt_admin
 
 # Register your models here.
 
 prompt_admin.register(User, UserAdmin)
+
+prompt_admin.register(Printer)
 
 prompt_admin.register(OneTimePassword)
