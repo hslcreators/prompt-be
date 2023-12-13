3c53af114fe0402a4c9414f1964480cbf2e8a22c
TSavage101 <bayodeiretomiwa@gmail.com>
2023-12-13 01:37:46 +0100

    perf: coverted db from postgres to sqlite

--- PromptBE/settings.py
+++ PromptBE/settings.py
@@ -116,9 +116,10 @@ WSGI_APPLICATION = 'PromptBE.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Custom User Model
