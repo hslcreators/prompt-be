84ee6ffb5e3cff6e6d2b71660b5b6a0798afe377
TSavage101 <bayodeiretomiwa@gmail.com>
2023-12-13 01:43:49 +0100

    feat: CORS and CSRF authorization feature updated

--- PromptBE/settings.py
+++ PromptBE/settings.py
@@ -84,12 +84,28 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     
-    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "http://127.0.0.1:5500",
     
-    "http://foroom.vercel.app",
-    "https://foroom.vercel.app",
+    "http://prompt.vercel.app",
+    "https://prompt.vercel.app",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    
+    "http://prompt.vercel.app",
+    "https://prompt.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'PromptBE.urls'
 
