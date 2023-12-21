959e3e9959d4ed5f82e4ed4303accf713060c6ab
TSavage101 <bayodeiretomiwa@gmail.com>
2023-12-21 02:48:04 +0100

    fix: Added code to save an order after creation; refactor: Re-arranged url routes

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.create_order, name="Create Order"),
-    path("<order_id>", views.get_order_by_id, name="Get Order By Order Id"),
     path("all-by-printer/", views.get_orders_by_printer, name="Get ALl Orders By Printer"),
-    path("all-by-user/", views.get_orders_by_user, name="Get ALl Orders By Users")
+    path("all-by-user/", views.get_orders_by_user, name="Get ALl Orders By Users"),
+    path("<order_id>/", views.get_order_by_id, name="Get Order By Order Id"),
 ]
