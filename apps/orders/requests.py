2e8712614a98985456dd820f9bb23d507b449c73
Marvellous <itzmhizterlouis@gmail.com>
2024-08-03 12:27:17 +0100

    Document field in documentation

--- apps/orders/requests.py
+++ apps/orders/requests.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 class CreateOrderRequest(serializers.Serializer):
+    printer_id = serializers.IntegerField()
     no_of_copies = serializers.IntegerField()
     pages = serializers.IntegerField()
     coloured = serializers.BooleanField()
-    document = serializers.FileField()
+    document = serializers.CharField(default="You actually need to add a file not a characters into this field")
     description = serializers.CharField()
     time_expected = serializers.TimeField()
 
