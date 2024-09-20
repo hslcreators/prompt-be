cf14d68814d98c75c9f80e03a45aae853d573ebd
Marvellous <itzmhizterlouis@gmail.com>
2024-09-20 13:31:36 +0100

    Changed conflicting url endpoints

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("<order_id>", views.get_order_by_id, name="Get Order By Order Id"),
     path("schedule/<order_id>", views.get_order_schedule, name="Get Order Schedule"),
     path("active/", views.get_active_orders, name="Get Active Orders"),
-    path("<order_id>/", views.update_complete_status, name="Update Order Status")
+    path("<order_id>/update", views.update_complete_status, name="Update Order Status")
 ]
